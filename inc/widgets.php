45be4bbafc3c75395364e8c123148602c23577a0
Martin Chorzewski <mchorzewski@whitelabelcoders.com>
2022-06-09 18:25:10 +0200

    init

---
+++ inc/widgets.php
@@ -0,0 +1,89 @@
+<?php
+
+class Search_Wine extends WP_Widget {
+	
+	public $attributes = array( 
+		'kraj',
+		'region',
+		'szczep',
+		'producent'
+	);
+	
+	public $attributes_terms =array();
+	
+	//setup
+	public function __construct() {
+		$widget_options = array(
+			'classname'   => 'search-wine',
+			'description' => 'Custom wine-search widget by Martin Chorzewski WLC',
+		);
+		parent::__construct( 'wine_searcher', 'Wine Searcher', $widget_options );
+	}
+	
+	//backend
+	public function form( $instance ) {
+		echo '<p>No options</p>';
+	}
+	
+	public function get_products_attrs() {
+		$attributes_array = array();
+		$attr;
+		
+		foreach( $this->attributes as $attribute ) {
+			$attributes_array[$attribute] = array();
+		}
+		
+		foreach( $this->attributes as $attribute ) {
+			if ( ! is_array( $attribute ) ) {
+				$attr = 'pa_' . $attribute;
+			}
+			array_push( $attributes_array[$attribute], get_terms( array(
+					'taxonomy'   => $attr,
+					'hide_empty' => false,
+				) ) );
+		}
+		return $attributes_array;
+	}
+	
+	//frontend
+	public function widget( $args, $instance ) {
+		
+		$this->attributes_terms = $this->get_products_attrs();
+		
+		echo $args['before_widget']; 
+		
+		$this->attributes_terms = $this->get_products_attrs(); ?>
+			<div class="wine-search__container">
+				<h2><?php _e( 'Znajdź Swoje Następne Wino' ) ?></h2>
+				<div class="wine-search__search">
+					<form class="wine-search__form" id="wine-search-form">
+						<input type="text"><?php
+						if ( ! empty( $this->attributes ) ) :
+							foreach( $this->attributes as $attribute ) : ?>
+								<label for="<?php esc_attr_e( $attribute ); ?>">
+									<select id="<?php esc_attr_e( $attribute ); ?>" name="<?php esc_attr_e( $attribute ); ?>" form="wine-search-form">
+										<option><?php _e( $attribute ); ?></option>
+										<?php foreach( $this->attributes_terms[$attribute] as $terms ) :
+											foreach( $terms as $term ) : ?>
+												<option value="<?php esc_attr_e( $term->name ); ?>">
+													<?php _e( $term->name ); ?>
+												</option>
+											<?php endforeach; ?>	
+										<?php endforeach; ?>
+									</select>
+								</label>
+							<?php endforeach; ?>
+						<?php endif; ?>
+						<input type="submit" value="Szukaj">
+					</form>
+				</div>
+			</div>
+		<?php
+		
+		echo $args['after_widget'];
+	}
+}
+
+add_action('widgets_init', function() {
+	register_widget( 'Search_Wine' );
+});
