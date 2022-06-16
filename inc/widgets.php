7d60a731857cbc67149b3c11af80c5ec37d775ca
Martin Chorzewski <mchorzewski@whitelabelcoders.com>
2022-06-16 14:34:30 +0200

    register on init

--- inc/widgets.php
+++ inc/widgets.php
@@ -2,12 +2,7 @@
 
 class Search_Wine extends WP_Widget {
 	
-	public $attributes = array( 
-		'kraj',
-		'region',
-		'szczep',
-		'producent'
-	);
+	public static $attributes = array();
 
 	public $wc_attributes;
 	
@@ -21,33 +16,38 @@ class Search_Wine extends WP_Widget {
 		);
 		parent::__construct( 'wine_searcher', 'Wine Searcher', $widget_options );
 	}
+
+	public static function init() {
+		register_widget( 'Search_Wine' );
+		self::fill_attributes_array();
+	} 
 	
 	//backend
 	public function form( $instance ) {
 		echo '<p>No options</p>';
 	}
 	
-	public function get_products_attrs() {
-		$attributes_array = array();
-		$attr;
+	// public static function get_products_attrs() {
+	// 	$attributes_array = array();
+	// 	$attr;
 		
-		foreach( $this->attributes as $attribute ) {
-			$attributes_array[$attribute] = array();
-		}
+	// 	foreach( self::$attributes as $attribute ) {
+	// 		$attributes_array[$attribute] = array();
+	// 	}
 		
-		foreach( $this->attributes as $attribute ) {
-			if ( ! is_array( $attribute ) ) {
-				$attr = 'pa_' . $attribute;
-			}
-			array_push( $attributes_array[$attribute], get_terms( array(
-					'taxonomy'   => $attr,
-					'hide_empty' => false,
-				) ) );
-		}
-		return $attributes_array;
-	}
+	// 	foreach( self::$attributes as $attribute ) {
+	// 		if ( ! is_array( $attribute ) ) {
+	// 			$attr = 'pa_' . $attribute;
+	// 		}
+	// 		array_push( $attributes_array[$attribute], get_terms( array(
+	// 				'taxonomy'   => $attr,
+	// 				'hide_empty' => false
+	// 			) ) );
+	// 	}
+	// 	return $attributes_array;
+	// }
 
-	public function get_wc_products() {
+	public static function wc_product_query() {
 
 		$products_array = array();
 
@@ -55,55 +55,50 @@ class Search_Wine extends WP_Widget {
 			'post_type'      => array('product'),
 			'post_status'    => 'publish',
 			'posts_per_page' => -1,
-		 );
+		);
 		
 		$query = new WP_Query( $args );
-		$product_ids = array();
-		$products = array();
-		 
-		 // The Loop
-		 if ( $query->have_posts() ): while ( $query->have_posts() ):
-			 $query->the_post();
-			 global $product;
-			 array_push( $product_ids, $product->get_attributes() );
-		 endwhile;
-			 wp_reset_postdata();
 
-		 endif;
-		 
-		 foreach( $product_ids as $id ) {
-			array_push( $products, wc_get_product( $id ) );
+		if ( $query->have_posts() ): while ( $query->have_posts() ):
+			$query->the_post();
+			global $product;
+		endwhile;
+		wp_reset_postdata();
+		endif;
+	
+		wp_reset_query();
+		return $product;
+	}
+
+	public static function get_product_attributes() {
+		$prd = self::wc_product_query();
+		return $prd->get_attributes();
+	}
+
+	public static function fill_attributes_array() {
+		$product_attributes = self::get_product_attributes();
+		foreach( $product_attributes as $attribute ) {
+			array_push( self::$attributes, substr( $attribute['name'], 3 ) );
 		}
-		print_r($product_ids);
-		 // TEST: Output the query IDs
-		
-		// while ( $query->have_posts() ) : $query->the_post();
-		// 	global $product;
-		// 	// array_push( $products_array,  $query);
-		// endwhile;
-
-		// wp_reset_query();
-
-		return $query;
 	}
 	
 	//frontend
 	public function widget( $args, $instance ) {
 		
-		$this->attributes_terms = $this->get_products_attrs();
+		// $this->attributes_terms = $this->get_products_attrs();
+		// $this->attributes_terms = $this->get_products_attrs(); 
 		
-		echo $args['before_widget']; 
-		
-		$this->attributes_terms = $this->get_products_attrs(); ?>
+		echo $args['before_widget'];  ?>
+
 		<div class="wine-search__container">
 			<h2><?php _e( 'Znajdź Swoje Następne Wino' ) ?></h2>
 			<div class="wine-search__search">
 				<form class="wine-search__form" id="wine-search-form" method="post">
 					<input type="text"><?php
-					if ( ! empty( $this->attributes ) ) :
-						foreach( $this->attributes as $attribute ) : ?>
+					if ( ! empty( self::$attributes ) ) :
+						foreach( self::$attributes as $attribute ) : ?>
 							<div class="<?php esc_attr_e( $attribute ); ?>">
-							<?php foreach( $this->attributes_terms[$attribute] as $terms ) :
+							<?php foreach( self::$attributes_terms[$attribute] as $terms ) :
 								foreach( $terms as $term ) : ?>
 								<label for="<?php esc_attr_e( $term->name ); ?>">
 									<input type="checkbox" value="<?php esc_attr_e( $term->name );?>" id="<?php esc_attr_e( $term->name ); ?>" name="<?php esc_attr_e( $attribute . "[]" ); ?>">
@@ -122,9 +117,6 @@ class Search_Wine extends WP_Widget {
 
 		<?php
 		if ( isset( $_POST['submit'] ) ) {
-
-			
-
 			
 			echo '<pre>';
 			print_r ( $_POST );
@@ -133,7 +125,7 @@ class Search_Wine extends WP_Widget {
 			// $product = $this->get_wc_products();
 			
 			echo '<pre>';
-			$this->get_wc_products();
+			print_r( self::$attributes );
 			echo '</pre>';
 		}
 
@@ -142,7 +134,4 @@ class Search_Wine extends WP_Widget {
 	
 }
 
-
-add_action('widgets_init', function() {
-	register_widget( 'Search_Wine' );
-});
+add_action( 'init', array( 'Search_Wine', 'init' ) );
